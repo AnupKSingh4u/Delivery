3bb37c52e3cb6d65f32538df49aa31addc1ab6d5
AnupKSingh4u <anupksingh4u@gmail.com>
2025-08-23 19:26:17 +0530

    First commit

---
+++ src/main/java/com/application/Calculator.java
@@ -0,0 +1,15 @@
+package com.application;
+
+public class Calculator {
+
+	  // Adds two numbers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Subtracts two numbers
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+	
+}
